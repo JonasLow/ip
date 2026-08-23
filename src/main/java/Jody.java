f118d050f9ab0960b921cfba2e3866248f55b230
JonasLow <e1137075@u.nus.edu>
2026-08-23 20:40:38 +0800

    Level-0: Greet the user and exit

---
+++ src/main/java/Jody.java
@@ -0,0 +1,17 @@
+public class Jody {
+    public static void main(String[] args) {
+        String banner = "     _           _       \n"
+                + "    | | ___   __| |_   _ \n"
+                + " _  | |/ _ \\ / _` | | | |\n"
+                + "| |_| | (_) | (_| | |_| |\n"
+                + " \\___/ \\___/ \\__,_|\\__, |\n"
+                + "                   |___/ \n";
+        System.out.println("____________________________________________________________");
+        System.out.println(banner);
+        System.out.println("Hello! I'm Jody.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
