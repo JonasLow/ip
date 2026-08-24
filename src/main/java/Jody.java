46e882cbb5189ace83c5690b3524bbfdac55384a
JonasLow <e1137075@u.nus.edu>
2026-08-24 20:40:07 +0800

    Level-2

--- src/main/java/Jody.java
+++ src/main/java/Jody.java
@@ -14,12 +14,22 @@ public class Jody {
         System.out.println("    What can I do for you?");
         System.out.println("    ____________________________________________________________\n");
 
+        String[] cmdList = new String[100];
+        int cmdCount = 0;
+
         while (true) {
             Scanner input = new Scanner(System.in);
             String cmd = input.nextLine();
-            if (!cmd.equalsIgnoreCase("bye")) {
+            if (cmd.equalsIgnoreCase("list")) {
                 System.out.println("    ____________________________________________________________");
-                System.out.println("    " + cmd);
+                for (int i = 1; i <= cmdCount; i++) {
+                    System.out.println("    " + i + ". " + cmdList[i - 1]);
+                }
+                System.out.println("    ____________________________________________________________\n");
+            } else if (!cmd.equalsIgnoreCase("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    added: " + cmd);
+                cmdList[cmdCount++] = cmd;
                 System.out.println("    ____________________________________________________________\n");
             } else {
                 System.out.println("    ____________________________________________________________");
