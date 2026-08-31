99b1e06f06a10e7a8a6c07112c4c3d15187b41b0
JonasLow <e1137075@u.nus.edu>
2026-08-31 13:13:18 +0800

    Modified code up to standard

--- src/main/java/Jody.java
+++ src/main/java/Jody.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Jody {
+
+    public static final int MAX_TASKS = 100;
+    public static final String divider = "    ____________________________________________________________";
     public static void main(String[] args) {
         String banner = "     _           _       \n"
                 + "    | | ___   __| |_   _ \n"
@@ -8,70 +11,73 @@ public class Jody {
                 + "| |_| | (_) | (_| | |_| |\n"
                 + " \\___/ \\___/ \\__,_|\\__, |\n"
                 + "                   |___/ \n";
-        System.out.println("    ____________________________________________________________");
+        System.out.println(divider);
         System.out.println(banner);
         System.out.println("    Hello! I'm Jody.");
         System.out.println("    What can I do for you?");
-        System.out.println("    ____________________________________________________________\n");
+        System.out.println(divider + "\n");
 
-        String[] cmdList = new String[100];
-        int cmdCount = 0;
-        String[] cmdState = new String[100];
+        String[] taskList = new String[MAX_TASKS];
+        int taskCount = 0;
+        String[] taskState = new String[MAX_TASKS];
 
-        for (int i = 0; i < 100; i++) {
-            cmdState[i] = "[ ]";
+        for (int i = 0; i < MAX_TASKS; i++) {
+            taskState[i] = "[ ]";
         }
 
+        Scanner input = new Scanner(System.in);
         while (true) {
-            Scanner input = new Scanner(System.in);
             String cmd = input.nextLine();
             if (cmd.equalsIgnoreCase("list")) {
-                System.out.println("    ____________________________________________________________");
+                System.out.println(divider);
                 System.out.println("    Here are the tasks in your list:");
-                for (int i = 1; i <= cmdCount; i++) {
-                    System.out.println("    " + i + "." + cmdState[i - 1] + " " + cmdList[i - 1]);
+                for (int i = 1; i <= taskCount; i++) {
+                    System.out.println("    " + i + "." + taskState[i - 1] + " " + taskList[i - 1]);
                 }
-                System.out.println("    ____________________________________________________________\n");
-            } else if (equalsMark(cmd, cmdCount)) {
-                System.out.println("    ____________________________________________________________");
+                System.out.println(divider + "\n");
+            } else if (isMark(cmd, taskCount)) {
+                System.out.println(divider);
                 System.out.println("    Nice! I've marked this task as done:");
                 int num = Integer.parseInt(cmd.split(" ")[1]) - 1;
-                cmdState[num] = "[X]";
-                System.out.println("      " + cmdState[num] + " " + cmdList[num]);
-                System.out.println("    ____________________________________________________________\n");
-            } else if (isUnmarked(cmd, cmdCount)) {
-                System.out.println("    ____________________________________________________________");
+                taskState[num] = "[X]";
+                System.out.println("      " + taskState[num] + " " + taskList[num]);
+                System.out.println(divider + "\n");
+            } else if (isUnmarked(cmd, taskCount)) {
+                System.out.println(divider);
                 System.out.println("    OK, I've marked this task as not done yet:");
                 int num = Integer.parseInt(cmd.split(" ")[1]) - 1;
-                cmdState[num] = "[ ]";
-                System.out.println("      " + cmdState[num] + " " + cmdList[num]);
-                System.out.println("    ____________________________________________________________\n");
+                taskState[num] = "[ ]";
+                System.out.println("      " + taskState[num] + " " + taskList[num]);
+                System.out.println(divider + "\n");
             } else if (!cmd.equalsIgnoreCase("bye")) {
-                System.out.println("    ____________________________________________________________");
+                System.out.println(divider);
                 System.out.println("    added: " + cmd);
-                cmdList[cmdCount++] = cmd;
-                System.out.println("    ____________________________________________________________\n");
+                taskList[taskCount++] = cmd;
+                System.out.println(divider + "\n");
             } else {
-                System.out.println("    ____________________________________________________________");
+                System.out.println(divider);
                 System.out.println("    Bye. Hope to see you again soon!");
-                System.out.println("    ____________________________________________________________\n");
+                System.out.println(divider + "\n");
                 break;
             }
         }
     }
 
-    private static boolean equalsMark(String cmd, int cmdCount) {
+    private static boolean isMark(String cmd, int cmdCount) {
         String[] words = cmd.split(" ");
-        if  (words.length != 2) {
+        if (words.length != 2) {
             return false;
         } else if (words[0].equalsIgnoreCase("mark")) {
             try {
                 int num = Integer.parseInt(words[1]);
-                if (num <= cmdCount) {
-                    return true;
-                } else {
-                    return false;
-                }
+                return num <= cmdCount;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        } else if (words[0].equalsIgnoreCase("unmark")) {
+            try {
+                int num = Integer.parseInt(words[1]);
+                return num <= cmdCount;
             } catch (NumberFormatException e) {
                 return false;
             }
@@ -86,11 +92,7 @@ public class Jody {
         } else if (words[0].equalsIgnoreCase("unmark")) {
             try {
                 int num = Integer.parseInt(words[1]);
-                if (num <= cmdCount) {
-                    return true;
-                } else {
-                    return false;
-                }
+                return num <= cmdCount;
             } catch (NumberFormatException e) {
                 return false;
             }
