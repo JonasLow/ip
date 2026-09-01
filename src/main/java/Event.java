fbbad771aa75bc3cc3b451a6e520d7ad5a2a6e4e
JonasLow <e1137075@u.nus.edu>
2026-09-02 00:39:58 +0800

    Added Event Class

---
+++ src/main/java/Event.java
@@ -0,0 +1,16 @@
+public class Event extends Task {
+    private final String from;
+    private final String to;
+
+    public Event(String description) {
+        super(description.split("/from")[0]);
+        this.from = "from: " + description.split("/from")[1];
+        this.to = "to: " + description.split("/to")[1];
+    }
+
+    @Override
+    public String toString() {
+        return ("[E]" + super.toString() + "\n" + "(" + this.from + this.to + ") \n");
+    }
+}
+
