32bd5684d3b137ec8d8caa21e15cf2cebebb7c67
JonasLow <e1137075@u.nus.edu>
2026-09-02 00:39:40 +0800

    Added Deadline Class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,13 @@
+public class Deadline extends Task {
+    private final String by;
+
+    public Deadline(String description) {
+        super(description.split("/by")[0]);
+        this.by =  "by: " + description.split("/by")[1];
+    }
+
+    @Override
+    public String toString() {
+        return("[D]" + super.toString() + "\n" + "(" + this.by + ") \n");
+    }
+}
