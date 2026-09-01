c21956d206734c6ed26b6a76ceeff7a06d7e97e5
JonasLow <e1137075@u.nus.edu>
2026-09-02 00:50:41 +0800

    Updated Deadline Class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -8,6 +8,6 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return("[D]" + super.toString() + "\n" + "(" + this.by + ") \n");
+        return("[D]" + super.toString() + "(" + this.by + ")");
     }
 }
